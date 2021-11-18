8072b877382147243f8e28c4243e1b8287e231b5
Christie Maduna <ferggiee@gmail.com>
2021-11-18 10:50:10 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,36 @@
 
   <div class="view chat" v-else>
     <header>
-      <button class="logout">Logout</button>
+      <button class="logout" @click="Logout">Logout</button>
       <h1>Welcome, {{ state.username }}</h1>
     </header>
-    <section class="chat-box">// Messages</section>
+
+    <section class="chat-box">
+      <div
+        v-for="message in state.messages"
+        :key="message.key"
+        :class="
+          message.username == state.username
+            ? 'message current-user'
+            : 'message'
+        "
+      >
+        <div class="message-inner">
+          <div class="username">{{ message.username }}</div>
+          <div class="content">{{ message.content }}</div>
+        </div>
+      </div>
+    </section>
+
     <footer>
-      <form @submit.prevent></form>
+      <form @submit.prevent="SendMessage">
+        <input
+          type="text"
+          v-model="inputMessage"
+          placeholder="Write a message..."
+        />
+        <input type="submit" value="Send" />
+      </form>
     </footer>
   </div>
 </template>
@@ -32,27 +56,57 @@
 <script>
 import { reactive, onMounted, ref } from "vue";
 import db from "./db";
-
 export default {
   setup() {
     const inputUsername = ref("");
-
+    const inputMessage = ref("");
     const state = reactive({
       username: "",
       messages: [],
     });
-
     const Login = () => {
       if (inputUsername.value != "" || inputUsername.value != null) {
         state.username = inputUsername.value;
         inputUsername.value = "";
       }
     };
-
+    const Logout = () => {
+      state.username = "";
+    };
+    const SendMessage = () => {
+      const messagesRef = db.database().ref("messages");
+      if (inputMessage.value === "" || inputMessage.value === null) {
+        return;
+      }
+      const message = {
+        username: state.username,
+        content: inputMessage.value,
+      };
+      messagesRef.push(message);
+      inputMessage.value = "";
+    };
+    onMounted(() => {
+      const messagesRef = db.database().ref("messages");
+      messagesRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        let messages = [];
+        Object.keys(data).forEach((key) => {
+          messages.push({
+            id: key,
+            username: data[key].username,
+            content: data[key].content,
+          });
+        });
+        state.messages = messages;
+      });
+    });
     return {
       inputUsername,
       Login,
       state,
+      inputMessage,
+      SendMessage,
+      Logout,
     };
   },
 };
