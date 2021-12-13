4ccecf231f9388b1edfe1904c5e7f0ee0694aa33
Christie Maduna <ferggiee@gmail.com>
2021-12-13 09:16:12 +0200

    commit

--- src/db.js
+++ src/db.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmw9-ruy7OilIrBtnFmJjmaMQ3YHK19p4",
+  apiKey: process.env.VUE_APP_API_KEY,
   authDomain: "firevuechat-898bb.firebaseapp.com",
   databaseURL: "https://firevuechat-898bb-default-rtdb.firebaseio.com",
   projectId: "firevuechat-898bb",
   storageBucket: "firevuechat-898bb.appspot.com",
-  messagingSenderId: "39789862104",
+  messagingSenderId: process.env.VUE_APP_SENDER_ID, 
   appId: "1:39789862104:web:134d333a11e3bd606a4bad",
 };
 
